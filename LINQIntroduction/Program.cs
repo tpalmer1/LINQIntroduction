041e0edfbc1a5a693c9797dbed69b10d2d14d7e0
Tyler Palmer <tpalmer12@email.davenport.edu>
2020-05-19 10:07:23 -0400

    Add project files.

---
+++ LINQIntroduction/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQIntroduction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] scores = new int[] { 97, 92, 81, 60 };
+
+            IEnumerable<int> scoreQuery =
+                from score in scores
+                where score > 80
+                orderby score ascending
+                select score;
+
+            /*IEnumerable<int> scoreQuery =
+                (from score in scores
+                 where score > 80
+                 select score).ToArray();*/
+
+            //Console.WriteLine("{0}",scoreQuery.Count());
+
+            //Immediate execution
+            int scoreCount = scoreQuery.Count();
+
+            //Deferred execution
+            foreach(int i in scoreQuery)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine("\nTotal: {0}",scoreCount);
+        }
+    }
+}
